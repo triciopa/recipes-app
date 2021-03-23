266fa1177e6368be44aeb6c013b9f66f3d95ce3a
Patricio <contacto@patriciogatti.ar>
2021-03-23 20:51:30 -0300

    Back-end index

--- api/index.js
+++ api/index.js
@@ -18,11 +18,76 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { conn, Diet } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('Server is listening at 3001'); // eslint-disable-line no-console
+    let gluten = Diet.findOrCreate({
+      where: {
+        name: 'gluten free',
+      },
+    });
+    let keto = Diet.findOrCreate({
+      where: {
+        name: 'ketogenic',
+      },
+    });
+    let dairy = Diet.findOrCreate({
+      where: {
+        name: 'dairy free',
+      },
+    });
+    let veggie = Diet.findOrCreate({
+      where: {
+        name: 'lacto ovo vegetarian',
+      },
+    });
+    let fodmap = Diet.findOrCreate({
+      where: {
+        name: 'fodmap friendly',
+      },
+    });
+    let vegan = Diet.findOrCreate({
+      where: {
+        name: 'vegan',
+      },
+    });
+    let pescatarian = Diet.findOrCreate({
+      where: {
+        name: 'pescatarian',
+      },
+    });
+    let paleo = Diet.findOrCreate({
+      where: {
+        name: 'paleolithic',
+      },
+    });
+    let primal = Diet.findOrCreate({
+      where: {
+        name: 'primal',
+      },
+    });
+    let whole = Diet.findOrCreate({
+      where: {
+        name: 'whole 30',
+      },
+    });
+
+    Promise.all([
+      gluten,
+      keto,
+      dairy,
+      veggie,
+      fodmap,
+      vegan,
+      pescatarian,
+      paleo,
+      primal,
+      whole,
+    ]).then((res) => {
+      console.log('Diets loaded');
+    });
   });
 });
