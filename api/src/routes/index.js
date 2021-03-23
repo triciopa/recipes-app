cf19faa3c94234bf0325dc26403eb947285740fa
Patricio <contacto@patriciogatti.ar>
2021-03-23 20:54:27 -0300

    Back-end routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,95 @@
+const { Sequelize } = require('sequelize');
+const { Recipe, Diet } = require('../db.js');
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require('axios');
+const { API_KEY } = process.env;
 
 const router = Router();
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const recipeRouter = require('./recipes');
+const dietRouter = require('./diets');
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/recipes', recipeRouter);
+router.use('/diets', dietRouter);
 
+router.get('/', async (req, res) => {
+  // const getData = async () => {
+  //   const request = await axios.get(
+  //     `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`
+  //   );
+  //   return request.data.results;
+  // };
+
+  // let data = await getData();
+
+  // for (let i = 0; i < data.length; i++) {
+  //   const element = data[i];
+  //   let instructions = '';
+  //   if (element.analyzedInstructions[0]) {
+  //     let steps = element.analyzedInstructions[0].steps;
+  //     for (let i = 0; i < steps.length; i++) {
+  //       const string = steps[i].step;
+  //       instructions = instructions + string;
+  //     }
+  //   } else {
+  //     instructions = null;
+  //   }
+  //   const newRecipe = await Recipe.findOrCreate({
+  //     where: {
+  //       title: element.title,
+  //     },
+  //     defaults: {
+  //       summary: element.summary,
+  //       score: element.spoonacularScore,
+  //       health: element.healthScore,
+  //       instructions: instructions,
+  //       spoonId: element.id,
+  //       image: element.image,
+  //     },
+  //   });
+
+  //   const diestDB = await Diet.findAll({
+  //     where: {
+  //       name: {
+  //         [Sequelize.Op.in]: element.diets,
+  //       },
+  //     },
+  //   });
+  //   await newRecipe[0].setDiets(dietsDB);
+  // } // ----- FOR END
+
+  console.log('Base de datos cargada');
+  res.send('OK');
+});
+
+router.post('/recipe', async (req, res) => {
+  let { title, summary, score, health, instructions, image, diets } = req.body;
+
+  const recipe = await Recipe.create({
+    title,
+    summary,
+    spoonacularScore: score,
+    healthScore: health,
+    instructions,
+    image,
+  });
+
+  if (!Array.isArray(diets)) {
+    diets = [diets];
+  }
+  const dietsDB = await Diet.findAll({
+    where: {
+      name: {
+        [Sequelize.Op.in]: diets,
+      },
+    },
+  });
+
+  await recipe.setDiets(dietsDB);
+  res.json(recipe);
+});
 
 module.exports = router;
---
+++ api/src/routes/recipes.js
@@ -0,0 +1,125 @@
+const router = require('express').Router();
+const axios = require('axios');
+const { Sequelize } = require('sequelize');
+const { Recipe, Diet } = require('../db.js');
+const { API_KEY } = process.env;
+
+router.get('/', async (req, res) => {
+  // console.log('Query', req.query);
+
+  const getData = async () => {
+    const request = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`
+    );
+    let filtered = await request.data.results.filter((elem) =>
+      elem.title.includes(req.query.name)
+    );
+    const newArray = await filtered.map((elem) => {
+      return {
+        id: elem.id * -1,
+        title: elem.title,
+        image: elem.image,
+        spoonacularScore: parseInt(elem.spoonacularScore),
+        diets: elem.diets.map((diet) => {
+          return { name: diet };
+        }),
+      };
+    });
+    return newArray;
+  };
+
+  try {
+    if (req.query) {
+      const dataRequired = {};
+      const name = req.query.name;
+      let search = await Recipe.findAll({
+        attributes: ['id', 'title', 'image', 'spoonacularScore'],
+        where: {
+          title: { [Sequelize.Op.like]: `%${name}%` },
+        },
+        include: {
+          model: Diet,
+          attributes: ['name'],
+          through: {
+            attributes: [],
+          },
+        },
+      });
+
+      search[0]
+        ? (dataRequired['results'] = search)
+        : (dataRequired['results'] = []);
+
+      const apiData = await getData();
+
+      // console.log(apiData[0]);
+
+      // adfadfa; // PROBAR ERROR 500 ---- descomentar
+
+      apiData[0]
+        ? (dataRequired.results = dataRequired.results.concat(apiData)) &&
+          res.json(dataRequired)
+        : dataRequired.results[0]
+        ? res.json(dataRequired)
+        : res.status(404).json({
+            results: {
+              error: 'No se encontraron datos que coincidan con la búsqueda',
+            },
+          });
+    } // ----------------- IF END
+  } catch (err) {
+    // console.log(err);
+    res.status(500).json({
+      results: {
+        error: 'Error de servidor, probablemente hayas consumido tus créditos',
+      },
+    });
+  }
+});
+
+router.get('/:idRecipe', async (req, res) => {
+  let id = req.params.idRecipe;
+
+  try {
+    if (id > 0) {
+      const search = await Recipe.findOne({
+        where: {
+          id: req.params.idRecipe,
+        },
+        include: {
+          model: Diet,
+          attributes: ['name'],
+          through: {
+            attributes: [],
+          },
+        },
+      });
+
+      res.json(search);
+    } else {
+      const request = await axios.get(
+        `https://api.spoonacular.com/recipes/${Math.abs(
+          id
+        )}/information?apiKey=${API_KEY}`
+      );
+      const obj = {
+        title: request.data.title,
+        summary: request.data.summary,
+        spoonacularScore: parseInt(request.data.spoonacularScore),
+        healthScore: parseInt(request.data.healthScore),
+        instructions: request.data.instructions,
+        image: request.data.image,
+        diets: request.data.diets,
+        diets: request.data.diets.map((diet) => {
+          return { name: diet };
+        }),
+      };
+      res.json(obj);
+    }
+  } catch (err) {
+    // console.log(err);
+    res.status(500).send('Error de servidor');
+  }
+});
+
+module.exports = router;
