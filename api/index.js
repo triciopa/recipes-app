dda81308b7a36f2394fe639eaf6f3ad5a9270d3f
Patricio <67854017+triciopa@users.noreply.github.com>
2021-05-16 12:19:34 -0300

    index and dependencies

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn, Diet } = require('./src/db.js');
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('Server is listening at 3001'); // eslint-disable-line no-console
     let gluten = Diet.findOrCreate({
       where: {
