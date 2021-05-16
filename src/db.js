8b584b20784d7e26f8924743f638898547fdd97e
Patricio <67854017+triciopa@users.noreply.github.com>
2021-05-16 15:32:54 -0300

    sequelize options included

--- src/db.js
+++ src/db.js
@@ -15,7 +15,16 @@ console.log(connectionString);
 const sequelize = new Sequelize(`${connectionString}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  options: {
+    dialect: 'postgres',
+    native: true,
+    ssl: true,
+    dialectOptions: {
+      ssl: true,
+    },
+  },
 });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
